548b15e2f27f7ef24b59fe29d9467121a539d21f
Yongjia Li <yoli@tcd.ie>
2022-03-31 21:02:13 +0100

    basic framework of main function

---
+++ src/mainProgram.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class mainProgram {
+
+    public static void main(String[] args) {
+        File file = new File("stop_times.txt");
+        boolean quit = false;
+        while (!quit) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println(
+                            "+------------ select function -----------+ \n" +
+                            "| 0 -> quit                              | \n" +
+                            "| 1 -> get shortest path between 2 stops | \n" +
+                            "| 2 -> search a stop name                | \n" +
+                            "| 3 -> search an arrival time            | \n" +
+                            "+----------------------------------------+");
+
+            if (sc.hasNextInt()) {
+                int function = sc.nextInt();
+                sc.nextLine();
+                if (function < 0 || function > 3) {
+                    System.out.println("invalid function select");              // handle invalid int input
+                } else {                                                        // normal case
+                    try {
+                        if (function == 0) {
+                            quit = true;
+                        } else if (function == 1) {
+
+                        } else if (function == 2) {
+
+                        } else {
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            } else {
+                System.out.println("please enter a number to select function"); // input is not integer
+            }
+
+        }
+    }
+
+}
